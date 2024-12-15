2bc27431777ce15bf55e0eb4735ef673fe0f8ed4
Oluwatimilehin <118487926+timilehin-code@users.noreply.github.com>
2024-12-15 13:58:47 +0100

    Add files via upload

---
+++ navbar/js/script.js
@@ -0,0 +1,7 @@
+ const hambuger = document.querySelector(".hambuger");
+ const navLinks = document.querySelector(".nav-links");
+ hambuger.addEventListener("click", ()=>{
+    navLinks.classList.toggle("open-nav");
+ })
+
+ 
